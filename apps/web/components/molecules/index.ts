fb460e001bd29d7eb7ac98efea0c81b3bd3988f5
AnamSadat <anamsadat3@gmail.com>
2025-12-15 01:42:21 +0700

    feat(component): update breadcrumb into navbar and update active state on sidebar content

--- apps/web/components/molecules/index.ts
+++ apps/web/components/molecules/index.ts
@@ -1,6 +1,9 @@
 export * from "@/components/molecules/button-theme";
 export * from "@/components/molecules/input-search";
 export * from "@/components/molecules/chart-area";
+export * from "@/components/molecules/overview-carousel";
+export * from "@/components/molecules/summary-cards";
+export * from "@/components/molecules/donut-chart";
 
 // ============== dir navbar ==================
 export * from "@/components/molecules/navbar/navbar-profile";
@@ -9,3 +12,16 @@ export * from "@/components/molecules/navbar/navbar-profile";
 export * from "@/components/molecules/sidebar/app-sidebar-header";
 export * from "@/components/molecules/sidebar/app-sidebar-footer";
 export * from "@/components/molecules/sidebar/app-sidebar-content";
+export * from "@/components/molecules/header";
+
+// ============= dir analisis-risiko ==================
+export * from "@/components/molecules/analisis-risiko";
+
+// ============= dir fasilitas-overview ==================
+export * from "@/components/molecules/fasilitas-overview";
+
+// ============= dir follow-up ==================
+export * from "@/components/molecules/follow-up";
+
+// ============= dir log-sistem ==================
+export * from "@/components/molecules/log-sistem";
